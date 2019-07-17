153f2c49b81669c7385fdf23602a58e66d83f2ac
dshtrikul <dshtrikul@users.noreply.github.com>
2019-07-17 22:41:14 +0300

    docker ready

--- flask_blog/configuration.py
+++ flask_blog/configuration.py
@@ -4,10 +4,10 @@ from flask_blog import app
 
 class Config():
     app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-    db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'site.db')
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+db_path
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'site.db')
+    #
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+db_path
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # run on first init
@@ -16,10 +16,10 @@ class Config():
     # falsk db upgrapde
 
 # MYSQL CONFIG
-    # DB_USERNAME = "root"
-    # DB_PASSWORD = "root"
-    # BLOG_DATABASE_NAME = 'blog_db'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@mysql:3306/{BLOG_DATABASE_NAME}"
+    DB_USERNAME = "root"
+    DB_PASSWORD = "root"
+    BLOG_DATABASE_NAME = 'blog_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@mysql:3306/{BLOG_DATABASE_NAME}"
 
 
     app.config['DEBUG'] = True
