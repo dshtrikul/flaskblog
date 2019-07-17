48aae08cc0055ba1af649e57ac1fe25b62f0ce14
dshtrikul <dshtrikul@users.noreply.github.com>
2019-07-17 22:25:40 +0300

    minor updates

--- flask_blog/configuration.py
+++ flask_blog/configuration.py
@@ -4,14 +4,24 @@ from flask_blog import app
 
 class Config():
     app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-    #some
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    DB_USERNAME = "root"
-    DB_PASSWORD = "root"
-    BLOG_DATABASE_NAME = 'blog_db'
+    db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'site.db')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@mysql:3306/{BLOG_DATABASE_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+db_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# run on first init
+    # flask db init
+    # flask db migrate
+    # falsk db upgrapde
+
+# MYSQL CONFIG
+    # DB_USERNAME = "root"
+    # DB_PASSWORD = "root"
+    # BLOG_DATABASE_NAME = 'blog_db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@mysql:3306/{BLOG_DATABASE_NAME}"
+
+
     app.config['DEBUG'] = True
     app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
     app.config['MAIL_PORT'] = 587
